2fc6b67ca145a2eeaa0ee3a890307641cd305ecc
XiaoCheng123 <1197138309@qq.com>
2016-09-02 17:49:55 +0800

    做了一些小修改

--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,8 +1,14 @@
 package com.coolweather.app.util;
 
+import com.coolweather.app.modei.CoolWeatherDB;
+
 public class Utility {
 
 	/**
-	 * 解析和处理服务器返回的省级数据
+	 * 解析和处理服务器返回的省级数据 
 	 */
+	public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB, String response){
+		
+		return false;
+	}
 }
