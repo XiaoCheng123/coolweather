49b184b1f1f9a02df7b00220b4f9051f0674052d
XiaoCheng123 <1197138309@qq.com>
2016-09-03 16:27:42 +0800

    增加了页面和activity

---
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -0,0 +1,78 @@
+package com.coolweather.app.activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import com.coolweather.app.R;
+import com.coolweather.app.modei.City;
+import com.coolweather.app.modei.CoolWeatherDB;
+import com.coolweather.app.modei.County;
+import com.coolweather.app.modei.Province;
+
+
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ChooseAreaActivity extends Activity {
+	
+	public static final int LEVEL_PROVINCE = 0;
+	public static final int LEVEL_CITY = 1;
+	public static final int LEVEL_COUNTY = 1;
+	
+	private ProgressDialog progressDialog;
+	private TextView titleText;
+	private ListView listView;
+	private ArrayAdapter<String> adapter;
+	private CoolWeatherDB coolWeatherDB;
+	private List<String> dataList = new ArrayList<String>();
+	/**
+	 * 省列表
+	 */
+	private List<Province> provincesList;
+	/**
+	 * 市列表
+	 */
+	private List<City> cittList;
+	/**
+	 * 县列表
+	 */
+	private List<County> countylList;
+	/**
+	 * 选中的省份
+	 */
+	private Province selectedProvince;
+	/**
+	 * 选中的城市
+	 */
+	private City selectedCity;
+	/**
+	 * 选中的等级
+	 */
+	private int currentLevel;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.choose_area);
+		listView = (ListView) findViewById(R.id.list_view);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- src/com/coolweather/app/util/Utility.java
+++ src/com/coolweather/app/util/Utility.java
@@ -1,6 +1,15 @@
 package com.coolweather.app.util;
 
+import java.util.concurrent.ConcurrentHashMap;
+
+import android.R.integer;
+import android.R.string;
+import android.text.TextUtils;
+
+import com.coolweather.app.modei.City;
 import com.coolweather.app.modei.CoolWeatherDB;
+import com.coolweather.app.modei.County;
+import com.coolweather.app.modei.Province;
 
 public class Utility {
 
@@ -8,7 +17,61 @@ public class Utility {
 	 * 解析和处理服务器返回的省级数据 
 	 */
 	public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB, String response){
-		
+		if (!TextUtils.isEmpty(response)) {
+			String[] allProvicnes = response.split(",");
+			for (String p : allProvicnes) {
+				String[] array = p.split("\\|");
+				Province province = new Province();
+				province.setProvinceCode(array[0]);
+				province.setProvinceName(array[1]);
+				//将解析处理的数据存储到Province表
+				coolWeatherDB.savePronvince(province);
+			}
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 解析和处理服务器返回的市级数据 
+	 */
+	public synchronized static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB, String response, int provinceId){
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCities = response.split(",");
+			if (allCities != null && allCities.length >0) {
+				for (String c : allCities) {
+					String[] array = c.split("\\|");
+					City city = new City();
+					city.setCityCode(array[0]);
+					city.setCityName(array[1]);
+					city.setProvinceId(provinceId);
+					//将解析出来的数据存储到County表
+					coolWeatherDB.saveCity(city);
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 解析和处理服务器返回的县级数据 
+	 */
+	public synchronized static boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB, String response, int cityId){
+		if (!TextUtils.isEmpty(response)) {
+			String[] allCounties = response.split(",");
+			if (allCounties != null && allCounties.length >0) {
+				for (String c : allCounties) {
+					String[] array = c.split("\\|");
+					County county = new County();
+					county.setCountyCode(array[0]);
+					county.setCountyName(array[1]);
+					county.setCityId(cityId);
+					//将解析出来的数据存储到County表
+					coolWeatherDB.saveCounty(county);
+				}
+			}
+			
+			return true;
+		}
 		return false;
 	}
 }
